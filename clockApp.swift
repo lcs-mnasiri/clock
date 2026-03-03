a1b261337adef00a4ee6a6f4d31641b27e60b481
Machid Nasiri <mnasiri@lcs.on.ca>
2026-03-02 20:13:29 -0500

    make new file and group

---
+++ clockApp.swift
@@ -0,0 +1,17 @@
+//
+//  clockApp.swift
+//  clock
+//
+//  Created by Machid on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct clockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
